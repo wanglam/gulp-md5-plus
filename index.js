ebbc7212fd65ea47a71b5da5eaff4e053d72e90f
王麟 <wanglin@ezbuy.com>
2015-08-07 14:05:14 +0800

    Remove Save Logical to gulp-md5-reger

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ var path = require('path')
 , fs = require('fs')
 , glob = require('glob');
 
-module.exports = function (size, ifile) {
+module.exports = function (size, ifile,destFile) {
     size = size | 0;
+    destFile = destFile===undefined?ifile:destFile;
+    
+    var isDestDirectory = fs.lstatSync(destFile).isDirectory();
+
     return through.obj(function (file, enc, cb) {
         if (file.isStream()) {
             this.emit('error', new gutil.PluginError('gulp-debug', 'Streaming not supported'));
@@ -33,26 +37,6 @@ module.exports = function (size, ifile) {
         var md5_filename = filename.split('.').map(function(item, i, arr){
             return i == arr.length-2 ? item + '_'+ d : item;
         }).join('.');
-
-        if(Object.prototype.toString.call(ifile) == "[object Array]"){
-            ifile.forEach(function(i_ifile){
-                i_ifile && glob(i_ifile,function(err, i_files){
-                    if(err) return console.log(err);
-                    i_files.forEach(function(i_ilist){
-                        var result = fs.readFileSync(i_ilist,'utf8').replace(new RegExp(filename), md5_filename);
-                        fs.writeFileSync(i_ilist, result, 'utf8');
-                    })
-                })
-            })
-        }else{
-            ifile && glob(ifile,function(err, files){
-                if(err) return console.log(err);
-                files.forEach(function(ilist){
-                    var result = fs.readFileSync(ilist,'utf8').replace(new RegExp(filename), md5_filename);
-                    fs.writeFileSync(ilist, result, 'utf8');
-                })
-            })
-        }
 
         file.path = path.join(dir, md5_filename);
 
